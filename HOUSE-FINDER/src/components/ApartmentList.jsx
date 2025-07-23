dfd3892da395143ca91ba777a067bb2ba22d9da4
JESSE MWENDWA NDUNDA <jessendunda2@gmail.com>
2025-07-23 11:44:44 +0300

    finish apartment list component

--- HOUSE-FINDER/src/components/ApartmentList.jsx
+++ HOUSE-FINDER/src/components/ApartmentList.jsx
@@ -1,11 +1,25 @@
-import React from "react"
-import ApartmentCard from "./ApartmentCard"
+import React from "react";
+import ApartmentCard from "./ApartmentCard";
 
-function ApartmentList ({apartments,toggleInterested,onSelect})
+function ApartmentList({ apartments, toggleInterested, onSelect }) {
+  if (apartments.length === 0) {
+    return <p>No apartments found. Please add some!</p>;
+  }
 
-if (apartments.length=== 0){
-    return
-     <p>
-        No apartments found .Please add some!
-     </p>
+  return (
+    <div className="apartmentList">
+      {apartments.map((apartment) => {
+        return (
+          <ApartmentCard
+            key={apartment.id}
+            apartment={apartment}
+            toggleInterested={toggleInterested}
+            onSelect={onSelect}
+          />
+        );
+      })}
+    </div>
+  );
 }
+
+export default ApartmentList;
