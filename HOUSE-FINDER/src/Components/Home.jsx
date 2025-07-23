64608b7bde4058fd9f8d301e50185132f4494437
JESSE MWENDWA NDUNDA <jessendunda2@gmail.com>
2025-07-23 15:27:33 +0300

    add styling to the home page

--- HOUSE-FINDER/src/Components/Home.jsx
+++ HOUSE-FINDER/src/Components/Home.jsx
@@ -9,4 +9,17 @@ function Home(){
       .then((res) => res.json())
       .then((data) => setApartments(data));
   }, []);
+
+  return (
+    <div style={{ padding: "1rem" }}>
+      <h1>Welcome to Nairobi Apartment Finder</h1>
+      <p>Find your next apartment with ease.</p>
+      <div style={styles.grid}>
+        {apartments.map((apt) => (
+          <ApartmentCard key={apt.id} apartment={apt} />
+        ))}
+      </div>
+    </div>
+  );
 }
+
