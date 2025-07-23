72937dd9fa768079a0850893d990b0fdfe6515ce
brenda njoki <ngunjiribrenda5@gmail.com>
2025-07-23 12:02:44 +0300

    Adds submission and Change inputs

--- HOUSE-FINDER/src/Components/ApartmentForm.jsx
+++ HOUSE-FINDER/src/Components/ApartmentForm.jsx
@@ -10,7 +10,45 @@ function addApartmentForm(ApartmentForm){
 
         };
 
+    const [formData, setFormData] =useState(emptyForm);
+    //handles changes
+    function handleChange(e){
+        const{name,value}= e.target;
 
+        setFormData({
+            ...formData,[name]:name ==="price"? Number(value):value
+        });
+    }
+    //handles form submission
+    function handleSubmit(e) {
+        e.preventDefault()
+
+        for(const key in formData){
+            if(!formData[key]){
+                alert("Please in fill all fields");
+                return;
+            }
+        }
+
+    }
+{
+
+}
+
+    return(
+        <form className="add-apartment-form">
+            <h2>Add new Apartment</h2>
+            //form inputs
+            <input type="text"placeholder="apartment-name" value={formData.name}onChange={handleChange}/>
+            <input type="text"placeholder="location"value={formData.location}onChange={handleChange}/>
+            <input type="number"placeholder="price"value={formData.price}onChange={handleChange}/>
+            <input type="url"placeholder="photos"value={formData.images}onChange={handleChange}/>
+            <textarea placeholder="description" value={formData.description}onChange={handleChange}/>
+            <button type="Submit">Add new Apartment</button>            
+          
+
+        </form>
+    )
        
 
     
