ac11ca66903c9782538c4196a64883fa5551353f
erskinenyongesawanami <erskineviper@gmail.com>
2025-07-23 12:19:41 +0300

    Initialize ApartmentCard component with basic layout

---
+++ HOUSE-FINDER/src/Components/ApartmentCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function ApartmentCard() {
+  return (
+    <div className="apartment-card">
+      <h3>Apartment Name</h3>
+      <p>Location</p>
+      <button>Interested?</button>
+    </div>
+  );
+}
+
+export default ApartmentCard;
