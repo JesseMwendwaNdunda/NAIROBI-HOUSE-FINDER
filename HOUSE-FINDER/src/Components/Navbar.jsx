4c7962b7ad83abde54fa39040940e44c3c34b30c
brenda njoki <ngunjiribrenda5@gmail.com>
2025-07-24 14:24:37 +0300

    Updates db.json

--- HOUSE-FINDER/src/Components/Navbar.jsx
+++ HOUSE-FINDER/src/Components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar(){
             
                 <li>
                     <a href="/home">Home</a>
-                    <a href="/about">About</a>
+                    <a href="/">About</a>
                     <a href="/add Apartment">Add Apartment</a>
                     <a href="/contact">Contact</a>
                 </li>
